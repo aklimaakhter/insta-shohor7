35e1ed071f2923b157f13db0e296edf807ae6c5e
aklimaakhter <aklimawebd@gmail.com>
2022-03-11 11:17:33 +0600

    liked post repeatation bug fixed

--- js/app.js
+++ js/app.js
@@ -144,6 +144,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML = "";
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -152,6 +153,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById("reported").innerHTML = "";
   const reportedPosts = getReportedPosts();
   // console.log(reportedPosts)
   reportedPosts.forEach((post) => {
